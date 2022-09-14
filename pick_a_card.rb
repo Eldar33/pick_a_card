848605268d6059444658a1deeb4816b146c5ea65
Eldar33 <eldar_elf_33@mail.ru>
2022-09-14 15:35:26 +1000

    replace suits - ♦, ♠, ♥ or ♣

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,6 +2,7 @@
 #commit other workplace
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+# suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['♠', '♥', '♣', '♦']
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
